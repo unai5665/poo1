7474450009e793e5f383adfba6f9a0c09833df78
unai5665 <unaiperez5665@gmail.com>
2024-02-27 13:39:49 +0100

    corregido

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-"""
+""""
 1. Qué palabra reservada se utiliza en lugar de animal (nombre de la clase) para acceder al atributo patas
 
 2. Qué palabra reservada hay que utilizar para crear un nuevo objeto
-"""
+"""""
 
 class animal:
     patas = 0
